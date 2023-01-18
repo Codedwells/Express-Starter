f192ff6facc43058c2d19b53491ea896f0dff866
coddedwells <wells.mail.13@gmail.com>
2023-01-18 23:25:16 +0300

    basic test endpoints

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express CRUD api made using typescript.
+
+This is a NODEjs backend api made with express and typescript.
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log('Hello world! ')
+import express, { NextFunction, Request, Response } from 'express';
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware with carrying technique
+const middleware = ({name}:{name:string}) => (req:Request,res:Response,next:NextFunction) =>{
+    res.locals.name =name;
+    next()
+}
+
+app.get('/', (req: Request, res: Response) => {
+	return res.send('hello world');
+});
+
+app.post('/api/v1/data', (req: Request, res: Response) => {
+	console.log(req.body);
+});
+
+app.get('api/v1/user',middleware({name:'Thomas'}), (req: Request<{name:string},{},{},{}>, res: Response) => {
+	res.send(`The users name is ${res.locals.name}`);
+});
+
+app.listen(PORT, () => {
+	console.log(`The server is listening on port ${PORT}`);
+});
