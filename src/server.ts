ee1de3eabc902f77726e98bc2c22e65a0f0f1b99
coddedwells <wells.mail.13@gmail.com>
2023-01-20 22:27:56 +0300

    home route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
-const PORT = 3000;
 const app = express();
+dotenv.config();
 
+let PORT = process.env.PORT;
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/',(req:Request,res:Response)=>{
+	console.log(req.body)
+
+	res.sendStatus(200)
+})
 
 app.listen(PORT, () => {
 	console.log(`The server is listening on port ${PORT}`);
