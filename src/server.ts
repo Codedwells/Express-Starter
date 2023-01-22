a47a7405b5492efb9141a97775e9fb2b392eab4a
coddedwells <wells.mail.13@gmail.com>
2023-01-22 11:40:02 +0300

    down chalk version

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import router from './routes/routes';
+import Logger from './library/Logger';
+import chalk from 'chalk';
 
 const app = express();
 dotenv.config();
@@ -15,6 +17,13 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/api/v1', router);
+
+//Logger.error("This is a test warning")
+const log= ()=>{
+	let log = console.log
+	  log(chalk.blue('Hello') + ' World' + chalk.red('!'))
+}
+log()
 
 app.listen(PORT, () => {
 	console.log(`The server is listening on port ${PORT}`);
