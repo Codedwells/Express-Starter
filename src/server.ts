246c7acc9ef058ac09da265077b7f89cf5bbaedb
Codedwells <wells.mail.13@gmail.com>
2024-05-27 22:38:06 +0300

    chore(core): update deps

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,46 @@
-import express, { NextFunction, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import compression from 'compression';
+import express, { NextFunction, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import compression from 'compression'
 
-import router from './routes/user.routes';
-import Logger from './library/Logger';
-import connectDb from './db.connect';
-import { config } from './config/config';
+import router from './routes/user.routes'
+import Logger from './library/Logger'
+import connectDb from './db.connect'
+import { config } from './config/config'
 
-dotenv.config();
- const app = express();
-const PORT = config.server.port;
+dotenv.config()
+const app = express()
+const PORT = config.server.port
 
 /** Middleware */
-app.use(compression());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(Logger.Borgen);
+app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(Logger.Borgen)
 
 /** Routes */
-app.use('/', router);
+app.use('/', router)
 
 /** Heathcheck */
 app.get('/ping', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ message: 'pong' });
-});
+    res.status(200).json({ message: 'pong' })
+})
 
 /** Error Handling */
 app.use((req, res, next) => {
-    Logger.error(`${req.path} Route not found!!!`);
+    Logger.error(`${req.path} Route not found!!!`)
 
-    next();
-});
+    next()
+})
 
 /**
  * Only start server if mongo connection success
  */
 const StartServer = () => {
     app.listen(PORT, () => {
-        Logger.info(`The server is listening on port ${PORT}`);
-    });
-};
-connectDb(StartServer);
+        Logger.info(`The server is listening on port ${PORT}`)
+    })
+}
+StartServer()
+//connectDb(StartServer);
 
 export default app
